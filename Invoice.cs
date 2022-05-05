41eba0de2d0a59b85d6fce45119d4ca585c89eb0
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-05-05 11:43:54 -0600

    Add project files.

---
+++ Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_1_LINQ1
+{
+    public class Invoice
+    {
+        private int quantityValue;
+        private decimal priceValue;
+
+        public int PartNumber { get; set; }
+
+        public string PartDescription { get; set; }
+
+        public Invoice(int part, string description, int count, decimal pricePerItem)
+        {
+            PartNumber = part;
+            PartDescription = description;
+            quantityValue = count;
+            Price = pricePerItem;
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantityValue;
+            }
+            set
+            {
+                if(value > 0)
+                {
+                    quantityValue = value;
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return priceValue;
+            }
+            set
+            {
+                if (value >= 0M)
+                {
+                    priceValue = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{PartNumber,-5}{PartDescription,-20}{Quantity,-5}{Price,6:C}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+
+namespace A2_1_LINQ1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var invoice = new[]
+            {
+                new Invoice(83,"Electric sander",7,57.98M),
+                new Invoice(24,"Power saw", 18,9.99M),
+                new Invoice(7,"Sledge hammer",11,21.50M),
+                new Invoice(77,"Hammer",76,11.99M),
+                new Invoice(39,"Lawn mower",3,79.50M),
+                new Invoice(68,"Screwdriver", 106,6.99M),
+                new Invoice(56, "Jig saw", 21, 11.00M),
+                new Invoice(3, "Wrench",34,7.50M)
+            };
+
+            Console.WriteLine("type 1 to sort by part description\ntype 2 to sort by price\ntype 3 to show only items and sort by quantity\ntype 4 to only show items sort by value\ntype 5 to sort items by value between $200 & $500");
+            int sss = Int32.Parse(Console.ReadLine());
+
+            if (sss == 1)
+            {
+                var bydes =
+                    from e in invoice
+                    orderby e.PartDescription
+                    select e;
+                Console.WriteLine("order by item\n");
+
+                foreach (var element in bydes)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+            else if (sss == 2)
+            {
+                var bypri =
+                    from e in invoice
+                    orderby e.Price
+                    select e;
+
+                Console.WriteLine("order by price\n");
+
+                foreach (var element in bypri)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+            else if (sss == 3)
+            {
+                var bypdq =
+                    from e in invoice
+                    orderby e.Quantity
+                    select e;
+
+                Console.WriteLine("order by quantity\n");
+
+                foreach( var element in bypdq)
+                {
+                    Console.WriteLine(element.Quantity + "    " + element.PartDescription);
+                }
+            }
+            else if (sss == 4)
+            {
+                var byqpa =
+                    from e in invoice
+                    orderby e.Quantity * e.Price
+                    select e;
+
+                Console.WriteLine("sorted by value\n");
+
+                foreach(var element in byqpa)
+                {
+                    Console.WriteLine(element + "   TotalValue: $" +(element.Quantity*element.Price));
+                }
+            }
+            else if (sss == 5)
+            {
+                var byqtp =
+                    from e in invoice
+                    where (e.Price * e.Quantity >= 200) && (e.Price * e.Quantity <= 500)
+                    select e;
+
+                foreach(var element in byqtp)
+                {
+                    Console.WriteLine(element+ "    TotalValue: $"+ (element.Price*element.Quantity));
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid integer submitted");
+            }
+        }
+    }
+}
